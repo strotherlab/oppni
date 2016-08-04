a64ee215cc57a7b91a5a26dda65cf96d7b28948c
Pradeep Reddy Raamana <raamana@gmail.com>
2016-08-03 17:13:22 -0700

    Bug fixes to make it work on a simple dataset ds003

--- bids/oppni.py
+++ bids/oppni.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import argparse
-import os
-import subprocess
+import os, sys
 from glob import glob
-import grabbids
 
-import cfg_front as cfg_oppni
-import proc_status_front as check_proc_status
+import subprocess
+
+# import grabbids
+# import cfg_front as cfg_oppni
+# import proc_status_front as check_proc_status
 
 global mcr_path, afni_path, fsl_path
 
@@ -62,24 +63,36 @@ def run_part_one(bids_dir, subject_label, task_name, output_dir):
     drop_end = 0
 
     # find all bold
+    epi_dir = os.path.join(bids_dir, "sub-%s" % subject_label, "func")
     func_pattern = "sub-{}_*task-{}*_bold.nii*".format(subject_label, task_name)
-    list_bold_nii = glob(os.path.join(bids_dir, "sub-%s" % subject_label, "func", func_pattern))
+    epi_list = glob(os.path.join(epi_dir, func_pattern))
 
+    anat_dir = os.path.join(bids_dir, "sub-%s" % subject_label, "anat")
     anat_pattern = "sub-{}_T1w.nii*".format(subject_label)
-    anat_file_list = glob(os.path.join(bids_dir, "sub-%s" % subject_label, "anat", anat_pattern))
+    anat_file_list = glob(os.path.join(anat_dir, anat_pattern))
 
     task_json = os.path.join(bids_dir,'task-{}_bold.json'.format(task_name))
-    events_tsv = os.path.join(bids_dir, "sub-%s_task-{}_events.tsv".format(subject_label,task_name))
+    events_tsv = os.path.join(epi_dir, "sub-{}_task-{}_events.tsv".format(subject_label, task_name))
 
-    for idx, func_file in enumerate(list_bold_nii):
-        # when multiple sessions exists:
-        # glob(os.path.join(args.bids_dir, "sub-%s" % subject_label, "ses-*", "func", "*_bold.nii*")
-        cmd = "run_oppni.sh %s PART1 %s %s %s %s %s %d %d %s %s" % (mcr_path, subject_label, func_file,
-                                                                 output_dir, anat_file_list[idx],
-                                                                 physio_file, drop_beg, drop_end, task_json,
-                                                                 events_tsv)
-        print(cmd)
-        #subprocess.run(cmd, shell=True, check=True)
+    # cmd = "run_oppni.sh %s PART1 %s %s %s %s %s %d %d %s %s" % (mcr_path, subject_label, func_file[0],
+    #                                                          output_dir, anat_file_list[0],
+    #                                                          physio_file, drop_beg, drop_end, task_json,
+    #                                                          events_tsv)
+
+    output_dir_sub = os.path.join(output_dir,"sub-%s" % subject_label)
+    cmd = "run_oppni.sh %s PART1 %s %s %s %s %d %d %s %s" % (mcr_path, epi_list[0],
+                                                                output_dir_sub, anat_file_list[0],
+                                                                physio_file, drop_beg, drop_end, task_json,
+                                                                events_tsv)
+
+    print(cmd)
+    try:
+        txt_out = subprocess.check_output(cmd, shell=True)
+        print txt_out
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
 
 def run_oppni():
     """Main function handling different levels of the OPPNI processing and analysis."""
@@ -92,9 +105,9 @@ def run_oppni():
 
     task_group = 'rhymejudgment'
 
-    status_check_dir = os.path.join(args.output_dir, 'status_checks')
-    if not os.path.exists(status_check_dir):
-        os.mkdir(status_check_dir)
+    # status_check_dir = os.path.join(args.output_dir, 'status_checks')
+    # if not os.path.exists(status_check_dir):
+    #     os.mkdir(status_check_dir)
 
     # ref_atlas = os.path.join(fsl_path,'data','standard','MNI152_T1_1mm.nii.gz')
 

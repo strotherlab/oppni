aa7f309e0d4b126561412849f933d61d169b2628
Mark Prati <mprati@research.baycrest.org>
2018-11-30 08:37:14 -0500

    JIT test #1

--- scripts_matlab/quick_lopass.m
+++ scripts_matlab/quick_lopass.m
@@ -9,13 +9,18 @@ function [ X_filt ] = quick_lopass( X, TR )
 % CODE_VERSION = '$Revision: 169 $';
 % CODE_DATE    = '$Date: 2014-12-15 18:09:33 -0500 (Mon, 15 Dec 2014) $';
 % ------------------------------------------------------------------------%
+inOctave = in_octave();
+if inOctave
+    val = jit_enable();
+    disp('Enableing Octave - JIT') 
+end
 
 [Nvox Ntime] = size(X); % matrix dimensions
 % using simple Butterworth filter -- linear phase/ flat frequency, rolloff not great but this is tolerable for fmri
 Wp = (2*TR)*0.08; % passband is below 0.08 Hz
 Ws = (2*TR)*0.10; % stopband is above 0.10 Hz
 % filter design: max passband attn. =50% / min stopband attn =1%
-inOctave = in_octave();
+
 if inOctave
     disp('Modified buttord call to custom buttord_octave version');
     [Nord, Wcut] = buttord_octave( Wp, Ws, 3,10 );

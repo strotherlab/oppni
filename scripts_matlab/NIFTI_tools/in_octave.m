d87fea240b4c3e180f156e67dd8500088647fb97
L. Mark Prati <mprati@research.baycrest.org>
2019-07-30 13:49:14 -0400

    gunzip support for octave

---
+++ scripts_matlab/NIFTI_tools/in_octave.m
@@ -0,0 +1,15 @@
+%
+% Simple function to check if we are are currently executing in Octave or Matlab   
+%
+% Mark Prati - mprati@research.baycrest.org 
+%
+function inOctave = in_octave()
+
+try
+    ver_num = OCTAVE_VERSION;
+    inOctave = 1;
+    version_str = ['OCTAVE ' ver_num];
+catch
+    inOctave = 0;
+    version_str  = ['MATLAB ' version];
+end

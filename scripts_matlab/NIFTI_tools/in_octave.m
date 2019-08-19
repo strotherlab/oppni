e979e25ca363bfbff79f2443f8e80920f2dfd62d
L. Mark Prati <mprati@research.baycrest.org>
2019-08-19 13:52:02 -0400

    tidy up

--- scripts_matlab/NIFTI_tools/in_octave.m
+++ scripts_matlab/NIFTI_tools/in_octave.m
@@ -1,9 +1,14 @@
 %
 % Simple function to check if we are are currently executing in Octave or Matlab   
 %
-% Mark Prati - mprati@research.baycrest.org 
+% USAGES: inOctave = in_octave();
+%         [inOctave,version_str] = in_octave();
+%         
+%  
+% Mark Prati (LMP)- mprati@research.baycrest.org 
 %
-function inOctave = in_octave()
+%
+function [inOctave,version_str] = in_octave()
 
 try
     ver_num = OCTAVE_VERSION;
